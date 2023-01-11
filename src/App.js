c0264f925fdeb87b488fc63c72c06e260f81f253
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-11 12:44:55 -0600

    created a basic info input form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
+import ContactInfo from "./components/ContactInfo";
 
 
-const App = () => {
-  return (
-    <Header />
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <ContactInfo />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/ContactInfo.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import '../styles/ContactInfo.css';
+
+class ContactInfo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            email: "",
+            phone: "",
+            address: "",
+            city: "",
+            state: "",
+            zip: "",
+            country: ""
+        };
+    }
+
+    render() {
+        return (
+            <div className="inputForm">
+                <form>
+                    <label>
+                        Name:
+                        <input type="text" name="name" />
+                    </label>
+                    <label>
+                        Email:
+                        <input type="text" name="email" />
+                    </label>
+                    <label>
+                        Phone:
+                        <input type="text" name="phone" />
+                    </label>
+                    <label>
+                        Address:
+                        <input type="text" name="address" />
+                    </label>
+                    <label>
+                        City:
+                        <input type="text" name="city" />
+                    </label>
+                    <label>
+                        State:
+                        <input type="text" name="state" />
+                    </label>
+                    <label>
+                        Zip:
+                        <input type="text" name="zip" />
+                    </label>
+                    <label>
+                        Country:
+                        <input type="text" name="country" />
+                    </label>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default ContactInfo;
