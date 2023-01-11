3257acd44ca12928eb607c2de223710151345cc3
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-11 12:06:10 -0600

    added a style subdirectory and styled the header for the application

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/Header.css';
+
+
+const Header = () => {
+    return (
+        <header>
+        <h1>Project: CV Aplication</h1>
+        </header>
+    );
+    }
+
+export default Header;
+
