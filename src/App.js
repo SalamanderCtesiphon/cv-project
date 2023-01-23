cfa0d63a6ecb13f1b94680d7233669e6b3b072bf
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-23 09:33:57 -0600

    added a page to display the completed cv

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import ContactInfo from "./components/ContactInfo";
 import Education from "./components/Education";
 import PracticalExperience from "./components/PracticalExperience";
+import Page from "./components/Page";
 
 class App extends React.Component {
     constructor(props) {
@@ -19,6 +20,7 @@ class App extends React.Component {
                 <ContactInfo />
                 <Education />
                 <PracticalExperience />
+                <Page />
             </div>
         );
     }
